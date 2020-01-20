7b0d49b8de6b6ea67000efb73434937919390749
Luca Hostettler <hostettler.luca1896@gmail.com>
2020-01-20 22:05:01 +0100

    Solve task 3 - percent colleges

--- universities.sh
+++ universities.sh
@@ -6,22 +6,22 @@
 # 23.01.2020 / Florian Bohren / Marc Bischof / Luca Hostettler
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     
-        machine=Linux
-        if dpkg -s csvkit 2>&1 ; then
-            echo "csvkit found"
-        else
-            echo "csvkit on linux not found"
-        fi
+Linux*)
+    machine=Linux
+    if dpkg -s csvkit 2>&1; then
+        echo "csvkit found"
+    else
+        echo "csvkit on linux not found"
+    fi
     ;;
-    Darwin*)    
-        machine=Mac
-        if brew ls --versions csvkit > /dev/null; then
-            echo "csvkit found"
-        else
-            echo "csvkit on mac not found"
-            exit 1
-        fi    
+Darwin*)
+    machine=Mac
+    if brew ls --versions csvkit >/dev/null; then
+        echo "csvkit found"
+    else
+        echo "csvkit on mac not found"
+        exit 1
+    fi
     ;;
 esac
 
@@ -43,8 +43,7 @@ menuCount=${#menu[@]}
 while true; do
     # Menu ausgeben
     echo "$TITLE"
-    for ((i=0; i<$menuCount; i++))  
-	do
+    for ((i = 0; i < $menuCount; i++)); do
         echo "$i) ${menu[$i]}"
     done
     # Eingabe verlangen und einlesen
@@ -52,50 +51,50 @@ while true; do
     read ANTWORT
     # case Anweisung - je nach Eingabe Verhalten bestimmen
     case $ANTWORT in
-        0) # Funktion Datenvorschau (Dataset Preview)
-            echo -e "\n=> ${MENU[0]}\n"
-            head -n 10 universities.csv | csvlook
-            echo ""
-        ;; 
-        1) # Funktion Daten Analyse
-            echo -e "\n=> ${MENU[1]}\n"
-			echo -e "Suchwort eingeben:"
-			read Suchwort
-			echo ""
-			csvgrep -c 1 -m $Suchwort universities.csv | csvlook
-			echo ""
+    0) # Funktion Datenvorschau (Dataset Preview)
+        echo -e "\n=> ${MENU[0]}\n"
+        head -n 10 universities.csv | csvlook
+        echo ""
         ;;
-        2) # Funktion: Anteil der Colleges
-            echo -e "\n=> ${MENU[2]}\n"
-			
-			# Menge aller Schulen				
-				schools=$(wc -l universities.csv | cut -c1-3)			
-			
-			# Anzahl Colleges
-				colleges=$(grep -i "college" universities.csv | grep -i -v "university" | wc -l)
-				
-			# Berechnung des Prozentsatzes:
-			#	zwischenresultat=$($colleges/schools)
-			#	$zwischenresultat x 100
-			#   
-          
+    1) # Funktion Daten Analyse
+        echo -e "\n=> ${MENU[1]}\n"
+        echo -e "Suchwort eingeben:"
+        read Suchwort
+        echo ""
+        csvgrep -c 1 -m $Suchwort universities.csv | csvlook
+        echo ""
         ;;
-		3) # Funktion: Universitäten eines Bundesstaates anzeigen.
-			echo -e "\n=> ${MENU[3]}\n"
-			echo -e "Suchwort eingeben (Bundesstaat):"
-			read State
-			cat universities.csv | cut -f1,3 -d, | sort -k 2 -t ',' | grep $State | csvlook
-		;;
-		4) # Funktion: Anzahl Universitäten eines Bundesstaates anzeigen.
-			echo -e "\n=> ${MENU[4]}\n"	
-			cat universities.csv | cut -f3 -d, | sort | uniq -c
-		;;
-		5) # Ende
-			echo -e "\n=> ${MENU[5]}\n"	
-			break # while Schleife beenden	
-		;;
-        *) # bei allen anderen Antworten kommt dieser Block zum Zug
-            echo -e "\n=> Ungueltige Eingabe\n"
+    2) # Funktion: Anteil der Colleges
+        echo -e "\n=> ${MENU[2]}\n"
+
+        # Menge aller Schulen
+        schools=($(wc -l universities.csv))
+        echo "school count $schools"
+        # Anzahl Colleges
+        colleges=($(csvgrep -c Name -r "(?i)college" universities.csv | wc -l))
+        echo "college count $colleges"
+        # Berechnung des Prozentsatzes:
+        zwischenresultat=($(bc <<< "scale=2; ($colleges/$schools)"))
+        # zwischenresultat=($("scale=2 ; ($colleges / $schools)" | bc))
+        prozent=($(bc <<< $zwischenresultat*100))
+        echo "$prozent% der schulen sind colleges"
+        ;;
+    3) # Funktion: Universitäten eines Bundesstaates anzeigen.
+        echo -e "\n=> ${MENU[3]}\n"
+        echo -e "Suchwort eingeben (Bundesstaat):"
+        read State
+        cat universities.csv | cut -f1,3 -d, | sort -k 2 -t ',' | grep $State | csvlook
+        ;;
+    4) # Funktion: Anzahl Universitäten eines Bundesstaates anzeigen.
+        echo -e "\n=> ${MENU[4]}\n"
+        cat universities.csv | cut -f3 -d, | sort | uniq -c
+        ;;
+    5) # Ende
+        echo -e "\n=> ${MENU[5]}\n"
+        break # while Schleife beenden
+        ;;
+    *) # bei allen anderen Antworten kommt dieser Block zum Zug
+        echo -e "\n=> Ungueltige Eingabe\n"
         ;;
     esac
 done
